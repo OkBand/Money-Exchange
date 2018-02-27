7ad38995e186290aac1f9dfd703a36ba70640dd3
OkBand <o.bandura42@gmail.com>
2018-02-27 22:29:42 +0300

    comments added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
     // Return an object containing the minimum number of coins needed to make change
-    var coinsObject = {};
+    var coinsObject = {};  //creating an empty object
 
     if(currency > 10000) {
-      coinsObject['error']= "You are rich, my friend! We don't have so much coins for exchange";
+      coinsObject['error']= "You are rich, my friend! We don't have so much coins for exchange"; //throwing an error. btw, it should be many, not much. unlike money, coins are countable
     }
     else {
       var coins = 0;
-      var change = currency;
+      var change = currency; //not obligatory, just to keep var names in order
 
       if(change >= 50) {
-        coins = Math.floor(change/50);
-        change = change % 50;
-        coinsObject['H'] = coins;
+        coins = Math.floor(change/50); //extracting the whole part of the money sum = how many 50 cent coins there are in the sum
+        change = change % 50; //finding the remainder = how much money is left
+        coinsObject['H'] = coins; //adding the key and its value to the object
       };
 
       if(change >= 25) {
@@ -41,29 +41,3 @@ module.exports = function makeExchange(currency) {
     };
     return coinsObject;
   };
-
-/*If the argument passed in is 0 or less, then the method should return an empty
-object. If the currency that you would like to exchange more than 10000 please
-return next object:
-{error: "You are rich, my friend! We don't have so much coins for exchange"}
-
-Write a method makeExchange that will determine the minimum number of coins
-needed to make change for a given amount in American currency. Coins used will
-be half-dollars, quarters, dimes, nickels, and pennies, worth 50¢, 25¢, 10¢, 5¢
-and 1¢, respectively. They'll be represented by the strings H, Q, D, N and P.
-
-The argument passed in will be an integer representing the value in cents.
-The return value should be an object with the symbols as keys, and the numbers
-of coins as values. Coins that are not used should not be included in the
-object.
-switch(expression) {
-    case n:
-        code block
-        break;
-    case n:
-        code block
-        break;
-    default:
-        code block
-}
-*/
